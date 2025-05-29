c0e2cbc91d1e9508894402894372fd68e48d01c9
Valerio Pio De Nicola <38079112+xV4L3x@users.noreply.github.com>
2025-05-29 19:48:32 +0200

    Add files via upload

---
+++ Tweak.h
@@ -0,0 +1,14 @@
+
+@interface UICalloutBar : UIView
+
+@property (nonatomic,readonly) bool isDisplayingVertically;
+@property (nonatomic, retain) NSArray *extraItems;
+@property (nonatomic, retain) UIMenuItem *slcOwOItem;
+
+@end
+
+@interface UICalloutBarButton : UIButton
+
+@property (nonatomic, assign) SEL action;
+
+@end
